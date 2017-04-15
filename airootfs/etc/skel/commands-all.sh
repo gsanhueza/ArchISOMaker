d6b5c88618b345f90be0acb30ff79ea9cc8458bf
gsanhueza <gabriel_8032@hotmail.com>
2017-04-15 16:29:09 -0300

    Fix pcmanfm para i3

--- airootfs/etc/skel/commands-all.sh
+++ airootfs/etc/skel/commands-all.sh
@@ -1,3 +1,3 @@
 cp /root/pacman_config_when_installed/pacman.conf /etc/pacman.conf -v
 cp /root/pacman_config_when_installed/mirrorlist /etc/pacman.d/mirrorlist -v
-pacstrap /mnt base base-devel yaourt vim grml-zsh-config gstreamer smplayer mplayer nvidia bumblebee refind-efi grub os-prober xorg xorg-xinit xorg-drivers cantarell-fonts gnome gnome-tweak-tool plasma kdebase kde-l10n-es virtualbox-guest-modules-arch virtualbox-guest-utils intel-ucode i3 dmenu feh lxdm-gtk3 lynx compton xterm alsa-utils
+pacstrap /mnt base base-devel yaourt vim grml-zsh-config gstreamer smplayer mplayer nvidia bumblebee refind-efi grub os-prober xorg xorg-xinit xorg-drivers cantarell-fonts gnome gnome-tweak-tool plasma kdebase kde-l10n-es virtualbox-guest-modules-arch virtualbox-guest-utils intel-ucode i3 dmenu feh lxdm-gtk3 lynx compton xterm pcmanfm-gtk3 alsa-utils
