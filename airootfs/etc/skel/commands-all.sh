3dbd1022bfdc54ded8f11a6afcab6a8e68e25d58
gsanhueza <gabriel_8032@hotmail.com>
2017-06-07 11:36:47 -0400

    Automatización de commands-all.sh

--- airootfs/etc/skel/commands-all.sh
+++ airootfs/etc/skel/commands-all.sh
@@ -1,8 +1,19 @@
+# Copy mirror file and pacman configuration
 echo "*** Installing packages from remote repos... ***"
 cp /root/pacman_config_when_installed/pacman.conf /etc/pacman.conf -v
 cp /root/pacman_config_when_installed/mirrorlist /etc/pacman.d/mirrorlist -v
-pacstrap /mnt base base-devel yaourt vim grml-zsh-config gstreamer smplayer nvidia bumblebee refind-efi grub os-prober xorg xorg-xinit xorg-drivers cantarell-fonts gnome gnome-tweak-tool plasma kdebase kde-l10n-es virtualbox-guest-modules-arch virtualbox-guest-utils intel-ucode i3 dmenu feh lxdm-gtk3 lynx compton xterm pcmanfm-gtk3 alsa-utils
 
+# Create obligatory directories
+newroot=/mnt
+echo "Creating install root at ${newroot}"
+mkdir -m 0755 -p "$newroot"/var/{cache/pacman/pkg,lib/pacman,log} "$newroot"/{dev,run,etc}
+mkdir -m 1777 -p "$newroot"/tmp
+mkdir -m 0555 -p "$newroot"/{sys,proc}
+
+# Pull packages
+pacman -Syw --root /mnt --cachedir /mnt/var/cache/pacman/pkg --noconfirm base base-devel yaourt vim grml-zsh-config gstreamer smplayer nvidia bumblebee refind-efi grub os-prober xorg xorg-xinit xorg-drivers cantarell-fonts gnome gnome-tweak-tool plasma kdebase kde-l10n-es virtualbox-guest-modules-arch virtualbox-guest-utils intel-ucode i3 dmenu feh lxdm-gtk3 lynx compton xterm pcmanfm-gtk3 alsa-utils
+
+# Copy packages to /media
 echo ""
 echo "*** Creating DB for all packages in /media ***"
 mkdir /media
@@ -10,6 +21,8 @@ mount.vboxsf pkg /media
 cp /mnt/var/cache/pacman/pkg /media -r
 echo "*** Syncing... ***"
 sync
+
+# Create DB for packages in /media
 echo "*** Creating new local DB... ***"
 repo-add /media/pkg/custom.db.tar.gz /media/pkg/*.xz
 sync
