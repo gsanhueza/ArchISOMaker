d3ffcd0c5c020663cf9174306a8de6fae1ce4cf8
gsanhueza <gabriel_8032@hotmail.com>
2017-06-11 19:43:47 -0400

    Fix legacy droppeado

--- airootfs/etc/skel/env.sh
+++ airootfs/etc/skel/env.sh
@@ -1,7 +1,7 @@
 export USERNAME="gabriel"
 export HOSTNAME="linux-pc"
 export LANGUAGE="es_CL"
-export DESKTOP_ENV="KDE" # KDE, GNOME, i3
+export DESKTOP_ENV="KDE" # KDE, GNOME 
 export BOOTLOADER="refind" # refind, grub
 export XORG_DRIVERS="nvidia" # nvidia, amd, vbox
 
