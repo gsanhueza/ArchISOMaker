9587a5793b3c77f4d0b274e9a9298d496122d4d8
Gabriel Sanhueza <gabriel_8032@hotmail.com>
2018-05-14 09:54:01 -0400

    Funciones para instalación creadas

--- airootfs/etc/skel/config.sh
+++ airootfs/etc/skel/config.sh
@@ -1,68 +1,109 @@
 source /root/env.sh
 
-echo "+++ Linking zoneinfo... +++"
-ln -s /usr/share/zoneinfo/America/Santiago /etc/localtime -f
+set_zoneinfo() 
+{
+    echo "+++ Linking zoneinfo... +++"
+    ln -s /usr/share/zoneinfo/$1 /etc/localtime -f
+}
 
-echo "+++ Setting time... +++"
-hwclock --systohc --utc
+enable_utc()
+{
+    echo "+++ Setting time... +++"
+    hwclock --systohc --utc
+}
 
-echo "+++ Enabling language and keymap... +++"
-sed -i "s/#\(${LANGUAGE}\.UTF-8\)/\1/" /etc/locale.gen
-echo "LANG=${LANGUAGE}.UTF-8" > /etc/locale.conf
-echo "KEYMAP=la-latin1" > /etc/vconsole.conf
-locale-gen
+set_language()
+{
+    echo "+++ Enabling language and keymap... +++"
+    sed -i "s/#\($1\.UTF-8\)/\1/" /etc/locale.gen
+    echo "LANG=$1.UTF-8" > /etc/locale.conf
+    echo "KEYMAP=la-latin1" > /etc/vconsole.conf
+    locale-gen
+}
 
-echo ""
-echo "+++ Creating hostname '${HOSTNAME}'... +++"
-echo $HOSTNAME > /etc/hostname
+set_hostname()
+{
+    echo ""
+    echo "+++ Creating hostname" $1 "... +++"
+    echo $1 > /etc/hostname
+}
 
-echo "+++ Enabling networking... +++"
-systemctl enable NetworkManager.service
+enable_networking()
+{
+    echo "+++ Enabling networking... +++"
+    systemctl enable NetworkManager.service
+}
 
-echo "+++ Enabling display manager... +++"
-if [ $DESKTOP_ENV == "KDE" ]
-then
-	systemctl enable sddm.service
-elif [ $DESKTOP_ENV == "GNOME" ]
-then
-	systemctl enable gdm.service
-elif [ $DESKTOP_ENV == "i3" ]
-then
-	systemctl enable lxdm.service
-fi
+enable_desktop_manager()
+{
+    echo "+++ Enabling display manager... +++"
+    if [ $DESKTOP_ENV == "KDE" ]
+    then
+    	systemctl enable sddm.service
+    elif [ $DESKTOP_ENV == "GNOME" ]
+    then
+    	systemctl enable gdm.service
+    elif [ $DESKTOP_ENV == "i3" ]
+    then
+    	systemctl enable sddm.service
+    fi
+}
 
-echo ""
-echo "+++ Creating linux image... +++"
-mkinitcpio -p linux
+make_linux_image()
+{
+    echo ""
+    echo "+++ Creating linux image... +++"
+    mkinitcpio -p linux
+}
 
-echo ""
-echo "+++ Setting root account... +++"
-chsh -s /bin/zsh
-passwd
+configure_root_account()
+{
+    echo ""
+    echo "+++ Setting root account... +++"
+    chsh -s /bin/zsh
+    passwd
+}
 
-echo ""
-echo "+++ Creating '${USERNAME}' account... +++"
-useradd -m -G wheel -s /bin/zsh $USERNAME 
-passwd $USERNAME 
+set_user_account()
+{
+    echo ""
+    echo "+++ Creating" $1 "account... +++"
+    useradd -m -G wheel -s /bin/zsh "$1"
+    passwd "$1"
 
-echo ""
-echo "+++ Enabling sudo for '${USERNAME}'... +++"
-sed -i 's/^#\s*\(%wheel\s\+ALL=(ALL)\s\ALL\)/\1/' /etc/sudoers
+    echo ""
+    echo "+++ Enabling sudo for" $1 "... +++"
+    sed -i 's/^#\s*\(%wheel\s\+ALL=(ALL)\s\ALL\)/\1/' /etc/sudoers
+}
 
-echo ""
-echo "+++ Installing ${BOOTLOADER} bootloader... +++"
+install_bootloader()
+{
+    echo ""
+    echo "+++ Installing" $1 "bootloader... +++"
+    
+    if [ "$1" == "grub" ]
+    then
+    	grub-install /dev/sda --force
+    	grub-mkconfig -o /boot/grub/grub.cfg
+    elif [ "$1" == "refind" ]
+    then
+    	mkdir -p /boot/EFI/refind # Mini-hack
+    	refind-install
+    	REFIND_UUID=$(cat /etc/fstab | grep UUID | grep "/ " | cut --fields=1)
+    	echo "\"Boot with standard options\"        \"rw root=${REFIND_UUID} initrd=/intel-ucode.img initrd=/initramfs-linux.img rcutree.rcu_idle_gp_delay=1 acpi_osi= acpi_backlight=native splash\"" > /boot/refind_linux.conf
+    fi
+}
 
-if [ $BOOTLOADER == "grub" ]
-then
-	grub-install /dev/sda --force
-	grub-mkconfig -o /boot/grub/grub.cfg
-elif [ $BOOTLOADER == "refind" ]
-then
-	mkdir -p /boot/EFI/refind # Mini-hack
-	refind-install
-	REFIND_UUID=$(cat /etc/fstab | grep UUID | grep "/ " | cut --fields=1)
-	echo "\"Boot with standard options\"        \"rw root=${REFIND_UUID} initrd=/intel-ucode.img initrd=/initramfs-linux.img rcutree.rcu_idle_gp_delay=1 acpi_osi= acpi_backlight=native splash\"" > /boot/refind_linux.conf
-fi
+set_zoneinfo "America/Santiago"
+enable_utc
+set_language $LANGUAGE
+set_hostname $HOSTNAME
+enable_networking
+enable_desktop_manager
+make_linux_image
+configure_root_account
+set_user_account $USERNAME
+install_bootloader $BOOTLOADER
 
 echo ""
 echo "+++++++++++++++++++++++++++++++++++++++++++++++"
--- airootfs/etc/skel/packages.sh
+++ airootfs/etc/skel/packages.sh
@@ -3,7 +3,7 @@ XORG="xorg xorg-drivers"
 
 GNOME="gnome gnome-tweak-tool"
 KDE="plasma kdebase kde-l10n-es okular gwenview spectacle smplayer"
-I3="i3 feh mc compton dmenu lxdm lxappearance lxterminal network-manager-applet gnome-keyring"
+I3="i3 feh mc compton dmenu wpa_supplicant dialog pulseaudio-alsa sddm qterminal networkmanager"
 
 REFIND="refind-efi"
 GRUB="grub os-prober"
