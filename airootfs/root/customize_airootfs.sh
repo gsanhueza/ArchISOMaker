42840928715a21740b3be14d449882c870dafd65
Gabriel Sanhueza <gabriel_8032@hotmail.com>
2020-07-07 14:32:50 -0400

    Updated for archiso 45-1

--- airootfs/root/customize_airootfs.sh
+++ airootfs/root/customize_airootfs.sh
@@ -5,8 +5,6 @@ set -e -u
 sed -i 's/#\(en_US\.UTF-8\)/\1/' /etc/locale.gen
 locale-gen
 
-ln -sf /usr/share/zoneinfo/UTC /etc/localtime
-
 usermod -s /usr/bin/zsh root
 cp -aT /etc/skel/ /root/
 mv /root/pacman_on_iso.conf /etc/pacman.conf
@@ -16,11 +14,3 @@ passwd -d root
 
 sed -i 's/#\(PermitRootLogin \).\+/\1yes/' /etc/ssh/sshd_config
 sed -i "s/#Server/Server/g" /etc/pacman.d/mirrorlist
-sed -i 's/#\(Storage=\)auto/\1volatile/' /etc/systemd/journald.conf
-
-sed -i 's/#\(HandleSuspendKey=\)suspend/\1ignore/' /etc/systemd/logind.conf
-sed -i 's/#\(HandleHibernateKey=\)hibernate/\1ignore/' /etc/systemd/logind.conf
-sed -i 's/#\(HandleLidSwitch=\)suspend/\1ignore/' /etc/systemd/logind.conf
-
-systemctl enable pacman-init.service choose-mirror.service systemd-networkd.service systemd-resolved.service
-systemctl set-default multi-user.target
