c669b9857ea40fdafd1c6d76d8183fc5b475dbba
Gabriel Sanhueza <gabriel_8032@hotmail.com>
2020-07-30 22:39:04 -0400

    Moved .zshrc from /etc/skel to /root, so customize_airootfs.sh can re-generate it

--- airootfs/root/customize_airootfs.sh
+++ airootfs/root/customize_airootfs.sh
@@ -8,5 +8,8 @@ echo 'Warning: customize_airootfs.sh is deprecated! Support for it will be remov
 
 sed -i 's/#\(en_US\.UTF-8\)/\1/' /etc/locale.gen
 locale-gen
+
 mv /root/pacman_on_iso.conf /etc/pacman.conf
+mv /root/zshrc /root/.zshrc
+
 sed -i "s/#Server/Server/g" /etc/pacman.d/mirrorlist
