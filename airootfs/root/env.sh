4d70ca1849277c4bc13af1f32403ac12c75e4d6d
Gabriel Sanhueza <gabriel_8032@hotmail.com>
2021-05-15 17:55:45 -0400

    Added more documentation in env.sh

--- airootfs/root/env.sh
+++ airootfs/root/env.sh
@@ -1,5 +1,8 @@
+# Settings for your installed system
+# Hint: To exit vim, press ESC + :wq + ENTER
+
 USERNAME="gabriel"              # Your username
-PASSWORD="arch"                 # Your password (placeholder, the script will prompt for this again)
+PASSWORD="arch"                 # Your password
 HOSTNAME="linux-pc"             # The hostname of this particular system
 LANGUAGE="es_CL"                # Check /etc/locale.gen for more options
 KEYMAP="la-latin1"              # Check /usr/share/kbd/keymaps/**/*.map.gz for more options
@@ -7,3 +10,4 @@ ZONEINFO="America/Santiago"     # Check /usr/share/zoneinfo/ for more options
 DESKTOP_ENV="KDE"               # KDE, GNOME, i3, X11
 BOOTLOADER="refind"             # refind, grub
 XORG_DRIVERS="nvidia"           # nvidia, amd, vbox, intel
+
