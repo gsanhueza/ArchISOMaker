30e7d5b4c6fb7976ea080b253044a488f9a6f87a
Gabriel Sanhueza <gabriel_8032@hotmail.com>
2021-05-15 15:30:34 -0400

    Removed interactive script, edit 'env.sh' now suggested on install

--- airootfs/root/install.sh
+++ airootfs/root/install.sh
@@ -96,197 +96,25 @@ configure_system()
     arch-chroot /mnt /bin/zsh -c "cd && ./$CONFFILE && rm $CONFFILE $ENVFILE -f"
 }
 
-prompt_username()
+prompt_environment()
 {
-    print_trailing "Enter your name (default=$USERNAME): "
-    read ans
-    case $ans in
-        '')
-            return
-        ;;
-        *)
-            export USERNAME=$ans
-        ;;
-    esac
-}
+    print_message "Your system will be installed using the data in '$ENVFILE'"
+    print_warning "Make sure your data is correct in $ENVFILE before proceeding!"
+    echo ""
 
-prompt_password()
-{
-    print_trailing "Enter your password (default=$PASSWORD): "
-    read -s ans
-    echo
-    case $ans in
-        '')
-            return
-        ;;
-        *)
-            export PASSWORD=$ans
-        ;;
-    esac
-}
-
-prompt_hostname()
-{
-    print_trailing "Enter your hostname (default=${HOSTNAME}): "
+    print_trailing "Do you want to install now? (y/N): "
     read ans
-    case $ans in
-        '')
-            return
-        ;;
-        *)
-            export HOSTNAME=$ans
-        ;;
-    esac
-}
 
-prompt_language()
-{
-    print_trailing "Enter your chosen language (default=$LANGUAGE): "
-    read ans
     case $ans in
-        '')
-            return
+        'y'|'Y')
+            print_success "Installation started."
         ;;
         *)
-            export LANGUAGE=$ans
-        ;;
-    esac
-}
-
-prompt_keymap()
-{
-    # Keymap
-    print_trailing "Enter your chosen keymap (default=$KEYMAP): "
-    read ans
-    case $ans in
-        '')
-            return
-        ;;
-        *)
-            export KEYMAP=$ans
-        ;;
-    esac
-}
-
-prompt_zoneinfo()
-{
-    print_trailing "Enter your chosen zoneinfo (default=$ZONEINFO): "
-    read ans
-    case $ans in
-        '')
-            return
-        ;;
-        *)
-            export ZONEINFO=$ans
-        ;;
-    esac
-}
-
-prompt_desktop_environment()
-{
-    # Desktop environment
-    print_light "Choose your Desktop Environment (default=$DESKTOP_ENV)"
-    print_trailing "(1) KDE    (2) GNOME    (3) i3    (4) X11: "
-    read ans
-    case $ans in
-        '')
-            return
-        ;;
-        '1')
-            export DESKTOP_ENV="KDE"
-        ;;
-        '2')
-            export DESKTOP_ENV="GNOME"
-        ;;
-        '3')
-            export DESKTOP_ENV="i3"
-        ;;
-        '4')
-            export DESKTOP_ENV="X11"
-        ;;
-        *)
-            print_failure "Wrong choice, halting now!"
+            print_failure "Installation stopped."
+            print_warning "Edit $ENVFILE and run this script again."
             exit 1
         ;;
     esac
-}
-
-prompt_bootloader()
-{
-    print_light "Choose your Bootloader (default=$BOOTLOADER)"
-    print_trailing "(1) rEFInd    (2) GRUB: "
-    read ans
-    case $ans in
-        '')
-            return
-        ;;
-        '1')
-            export BOOTLOADER="refind"
-        ;;
-        '2')
-            export BOOTLOADER="grub"
-        ;;
-        *)
-            print_failure "Wrong choice, halting now!"
-            exit 1
-        ;;
-    esac
-}
-
-prompt_video_drivers()
-{
-    print_light "Choose your Graphic Drivers (default=$XORG_DRIVERS)"
-    print_trailing "(1) nVidia    (2) AMD    (3) VBox    (4) Intel: "
-    read ans
-    case $ans in
-        '')
-            return
-        ;;
-        '1')
-            export XORG_DRIVERS="nvidia"
-        ;;
-        '2')
-            export XORG_DRIVERS="amd"
-        ;;
-        '3')
-            export XORG_DRIVERS="vbox"
-        ;;
-        '4')
-            export XORG_DRIVERS="intel"
-        ;;
-        *)
-            print_failure "Wrong choice, halting now!"
-            exit 1
-        ;;
-    esac
-}
-
-export_environment()
-{
-    # First 'echo' uses a single ">" because we want to re-write the file
-    echo "USERNAME=$USERNAME" > $ENVFILE
-    echo "PASSWORD=$PASSWORD" >> $ENVFILE
-    echo "HOSTNAME=$HOSTNAME" >> $ENVFILE
-    echo "LANGUAGE=$LANGUAGE" >> $ENVFILE
-    echo "KEYMAP=$KEYMAP" >> $ENVFILE
-    echo "ZONEINFO=$ZONEINFO" >> $ENVFILE
-    echo "DESKTOP_ENV=$DESKTOP_ENV" >> $ENVFILE
-    echo "BOOTLOADER=$BOOTLOADER" >> $ENVFILE
-    echo "XORG_DRIVERS=$XORG_DRIVERS" >> $ENVFILE
-}
-
-customize_env() {
-    print_message "This installation script will ask for some data before installing.\n"
-
-    prompt_username
-    prompt_password
-    prompt_hostname
-    prompt_language
-    prompt_keymap
-    prompt_zoneinfo
-    prompt_desktop_environment
-    prompt_bootloader
-    prompt_video_drivers
 }
 
 check_mounted_drive() {
@@ -323,44 +151,13 @@ verify_installation()
     [[ ! -f /mnt/root/$CONFFILE && ! -f /mnt/root/$ENVFILE && ! -f /mnt/root/$PRINTERFILE ]]
 }
 
-parse_arguments()
-{
-    while [[ $# -gt 0 ]]
-    do
-        key=$1
-        case $key in
-            -d|--direct)
-                export INTERACTIVE=0
-                shift # past argument
-            ;;
-            -k|--no-reboot)
-                export AUTOREBOOT=0
-                shift # past argument
-            ;;
-            *) # unknown option
-                print_failure "Usage: $SCRIPTFILE [-d | --direct] [-k | --no-reboot]"
-                exit 1
-            ;;
-        esac
-    done
-}
-
 main()
 {
     # Check pre-install state
-    parse_arguments $@
     check_mounted_drive
 
-    # Install as script or interactively
-    if [[ $INTERACTIVE == 1 ]]; then
-        customize_env
-    else
-        print_warning "Installing with defaults set in $ENVFILE..."
-        prompt_password
-    fi
-
-    # Save data entered by the user
-    export_environment
+    # Prompt user to check environment file before installing
+    prompt_environment
 
     # Install and verify
     install_system
