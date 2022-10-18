2b148d6ef7b490bd246d6214789d2e2ecf2f8b96
Gabriel Sanhueza <gabriel_8032@hotmail.com>
2022-10-18 15:33:51 -0300

    Updated customize_airootfs.sh to enable NetworkManager on boot

--- airootfs/root/customize_airootfs.sh
+++ airootfs/root/customize_airootfs.sh
@@ -6,9 +6,16 @@ set -e -u
 
 # Warning: customize_airootfs.sh is deprecated! Support for it will be removed in a future archiso version.
 
+# Generate US locale
 sed -i 's/#\(en_US\.UTF-8\)/\1/' /etc/locale.gen
 locale-gen
 
+# Enable mirrorlists
 sed -i "s/#Server/Server/g" /etc/pacman.d/mirrorlist
+
+# Avoid running reflector
 systemctl mask reflector.service
+
+# Enable NetworkManager
+systemctl enable NetworkManager.service
 
